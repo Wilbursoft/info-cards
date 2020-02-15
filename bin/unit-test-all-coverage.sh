31dd92fc837b502bb0a37c82d9793e91a4cf1e9a
GuyRoberts1972 <guywilliamwelchroberts@gmail.com>
2020-02-15 12:33:17 +0000

    initial put

--- bin/unit-test-all-coverage.sh
+++ bin/unit-test-all-coverage.sh
@@ -3,5 +3,5 @@
 # Run php unit
 plugindir=.
 phpbindir=~/environment/vendor/bin
-$phpbindir/phpunit  --bootstrap $plugindir/tests/bootstrap.php --stop-on-failure --stop-on-warning --coverage-html  $plugindir/build-output/coverage   --whitelist $plugindir
+$phpbindir/phpunit  --bootstrap $plugindir/tests/bootstrap.php --stop-on-failure --stop-on-warning --coverage-html  $plugindir/build/coverage   --whitelist $plugindir
 
